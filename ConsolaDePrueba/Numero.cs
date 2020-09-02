90553e39d120bae17085b887c0fdefbb544782b7
Sebastian_Milian <milianmejiaedgarsebastian@yahoo.com>
2020-09-02 15:20:06 -0600

    commit para merge en master

---
+++ ConsolaDePrueba/Numero.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsolaDePrueba
+{
+    class Numero : IComparable
+    {
+        public int valor { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            var temp = obj as Numero;
+
+            if (this.valor == temp.valor)
+            {
+                return 0;
+            }
+            else if (this.valor > temp.valor)
+            {
+                return 1;
+            }
+            if (this.valor < temp.valor)
+            {
+                return -1;
+            }
+
+            return 2;
+        }
+    }
+}
--- ConsolaDePrueba/Program.cs
+++ ConsolaDePrueba/Program.cs
@@ -1,12 +1,68 @@
 ﻿using System;
-
+using ArbolMulticamino;
 namespace ConsolaDePrueba
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            ArbolMultiCamino<Numero> Arbolin = new ArbolMultiCamino<Numero>(3);
+            
+            int decision;
+            bool terminarCiclo = false;
+
+            while (terminarCiclo == false)
+            {
+                Console.WriteLine("Eliga una opcion");
+                Console.WriteLine("1. Insertar");
+                Console.WriteLine("2. InOrden");
+                Console.WriteLine("3. Mostrar");
+
+                Console.WriteLine("0 salir");
+                decision = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                switch (decision)
+                {
+                    case 1:
+                        Numero nuevo = new Numero();
+                        Console.WriteLine("Ingrese el numero");
+                        nuevo.valor = Convert.ToInt32(Console.ReadLine());
+                        Arbolin.Insertar(nuevo);
+                        break;
+                    case 2:
+                        Arbolin.InOrden(Arbolin.Raiz);
+                        foreach (var item in Arbolin.RecolectorRecorridos)
+                        {
+                            Console.WriteLine(item.valor);
+                        }
+                        Console.ReadLine();
+                        break;
+                    case 0:
+                        terminarCiclo = true;
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.Clear();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
