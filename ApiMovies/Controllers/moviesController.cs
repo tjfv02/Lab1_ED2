9de32d8fe180c6f9208876c7f41913d36901738c
Sebastian_Milian <milianmejiaedgarsebastian@yahoo.com>
2020-09-09 16:22:54 -0600

    Creacion de api, post para iniciar arbol, clase movie con su comparador

---
+++ ApiMovies/Controllers/moviesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ArbolMulticamino;
+using ApiMovies.Models;
+
+namespace ApiMovies.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class moviesController : ControllerBase
+    {
+
+        static ArbolMultiCamino<Movie> Arbol;
+        static bool ArbolInicialiado = false;
+
+        [HttpPost]
+        public string InicializarArbol(int order)
+        {
+            var mensaje = "ArbolInicializado";
+            Arbol = new ArbolMultiCamino<Movie>(order);
+            ArbolInicialiado = true;
+            return mensaje;
+        }
+        
+
+    }
+}
---
+++ ApiMovies/Models/Movie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiMovies.Models
+{
+    public class Movie : IComparable
+    {
+        public string Director { get; set; }
+        public float ImdbRating{ get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int RottenTomatoesRating { get; set; }
+        public string Title { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            var temp = obj as Movie;
+
+            if (string.Compare(this.Title, temp.Title) == 0)
+            {
+                return 0;
+            }
+            else if (string.Compare(this.Title, temp.Title) == 1)
+            {
+                return 1;
+            }
+            if (string.Compare(this.Title, temp.Title) == -1)
+            {
+                return -1;
+            }
+
+            return 2;
+        }
+
+
+    }
+}
